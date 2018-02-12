87e1752b1cb19cf4c2c4a5e527b70e9ce44b59b3
Juan Chaves <juanchaves@MacBook-Pro-de-Juan.local>
2018-02-12 09:54:12 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var scrolled=0;
+function main(){
+	$(".descripcion").hide();
+	 function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top},
+        'slow');
+}
+
+$(".options > a").click(function(e) { 
+      // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+      // Call the scroll function
+    goToByScroll(this.id);           
+});
+}
+$(document).ready(main);
+
